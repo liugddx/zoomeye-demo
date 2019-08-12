b8ca66a4a8bce641da902244b0584a71ad587c05
liugd <liugd@si-tech.com.cn>
2019-08-12 09:11:59 +0800

    add swagger

--- src/main/java/com/liugdxd/zoomeye/ZoomeyeDemoApplication.java
+++ src/main/java/com/liugdxd/zoomeye/ZoomeyeDemoApplication.java
@@ -2,8 +2,10 @@ package com.liugdxd.zoomeye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ZoomeyeDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/liugdxd/zoomeye/configuration/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.liugdxd.zoomeye.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @ClassName SwaggerConfiguration
+ * @Description swagger配置类
+ * @Author liugd
+ * @Date 2019/8/11 23:06
+ * @Version 1.0
+ */
+@Configuration
+public class SwaggerConfiguration {
+
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     *
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.liugdxd"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("钟馗之眼数据收集demo")
+                .description("钟馗之眼数据收集demo")
+                .termsOfServiceUrl("https://www.zoomeye.org")
+                .version("1.0")
+                .build();
+    }
+}
